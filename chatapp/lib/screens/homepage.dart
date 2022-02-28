d8485b954c0c1dedba2dc3769a8b8742b638949f
jnguyen143 <jnguyen143@student.gsu.edu>
2022-02-28 15:02:55 -0500

    Started basic setup for chat app

---
+++ chatapp/lib/screens/homepage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Welcome!"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Form(
+                child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Logout Confirmation'),
+                  content: const Text('Are you sure you want to log out?'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                      child: const Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ),
+              child: const Text("Logout"),
+            )
+          ],
+        ))));
+  }
+}
